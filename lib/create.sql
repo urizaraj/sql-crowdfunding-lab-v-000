174afb62b2e9e169e97ff56c919504cdf54f2745
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 16:14:35 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,21 @@
+create table projects (
+  id primary key integer,
+  title text,
+  category text,
+  funding_goal integer,
+  start_date integer,
+  end_date integer
+);
+
+create table users (
+  id primary key integer,
+  name text,
+  age integer
+);
+
+create table pledges (
+  id primary key integer,
+  amount integer,
+  user integer,
+  project integer
+);
