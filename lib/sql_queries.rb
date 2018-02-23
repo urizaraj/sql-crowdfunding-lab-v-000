3c38c4c65a68105e99087adb5b65c0870376eac9
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 16:47:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  'SELECT title FROM projects LEFT JOIN pledges ON projects.id = pledges.project_id HAVING SUM(pledges.amount) > funding_goal GROUP BY title'
+  'SELECT title FROM projects LEFT JOIN pledges ON projects.id = pledges.project_id GROUP BY title HAVING SUM(pledges.amount) > funding_goal'
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
