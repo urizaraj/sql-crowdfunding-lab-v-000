d32525ef136476e8192e9d50f88e49637ffa44f5
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 16:58:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
     LEFT JOIN projects
     ON pledges.project_id = projects.id
     WHERE projects.category = "music"
-  ]
+  ].join(' ')
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
